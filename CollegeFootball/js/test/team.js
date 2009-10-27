7e8a6e49067d547d92e5a3dd38be112082504901
smith <nlloyds@gmail.com>
2009-10-26 21:36:06 -0500

    updated date.js

--- CollegeFootball/js/test/team.js
+++ CollegeFootball/js/test/team.js
@@ -2,6 +2,6 @@ module("team");
 
 test("date formatting", function () {
     expect(1);    
-    equals(Date.parse("12:30 PM"), Date.today().addHours(12.5), 
+    same(Date.parse("12:30 PM"), Date.today().addHours(12.5), 
         "12:30 PM gets parsed")
 });

81efc241cb4fb45646b85babf51c62f0a43e4c52
Nathan L Smith <nlloyds@gmail.com>
2011-08-29 22:05:08 -0500

    make build just zip

--- build.sh
+++ build.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-# Zip up widget for distribution. Requires zip and svn
+# Zip up widget for distribution.
 
 PROJ=CollegeFootball.wdgt
-REPO=`svn info | grep ^URL - | sed -e s/URL:\ //g -`/$PROJ # Get the repo URL
-TEMP=tmp
-ZIPFILE=$PROJ.zip
 
-echo "Exporting project..."
-svn export $REPO $TEMP/$PROJ
 echo "Creating archive..."
-cd $TEMP
-zip -r $ZIPFILE $PROJ
-mv $ZIPFILE ..
-cd ..
-echo "Deleting temporary files..."
-rm -rfv $TEMP
+zip -r $PROJ.zip $PROJ
 echo "Created $ZIPFILE. Done."

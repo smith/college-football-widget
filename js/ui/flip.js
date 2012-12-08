b5ed122dc73ea26549f595b7346cdde917c94851
Nathan L Smith <nlloyds@gmail.com>
2012-12-08 00:17:08 -0600

    remove most widget references

--- js/ui/flip.js
+++ js/ui/flip.js
@@ -1,27 +1,9 @@
-// Provides an abstraction for flipping the widget over on the Mac Dashboard or
-// a web context
+// Provides an abstraction for flipping over
 define(function () {
     return {
         perform: function (front, back, event) {
-            var toSide, w = window.widget;
-            if (w) {
-                toSide = front.is(":visible") ? "ToBack" : "ToFront";
-                w.prepareForTransition(toSide);
-                if (toSide === "ToFront") {
-                    front.height(back.height());
-                    back.hide();
-                    front.show();
-                } else {
-                    // Normalize height
-                    back.height(front.height());
-                    front.hide();
-                    back.show();
-                }
-                setTimeout(function () { w.performTransition(); }, 0);
-            } else {
-                front.toggle();
-                back.toggle();
-            }
+            front.toggle();
+            back.toggle();
             return false;
         }
     };
